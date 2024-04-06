fb363c7c353c429892451ffeada682ccc23112a2
Pavithra Dhanakotti <dhanakotipavithra26@gmail.com>
2024-04-06 14:22:31 +0530

    Updated program

---
+++ src/main/java/org/WebAutomation/pages/DashboardPage_POM.java
@@ -0,0 +1,22 @@
+package org.WebAutomation.pages;
+import org.WebAutomation.base.CommonToAllPage;
+import org.openqa.selenium.By;
+public class DashboardPage_POM extends CommonToAllPage{
+    public DashboardPage_POM(){
+
+    }
+
+
+    // Page Locators
+    By userNameOnDashboard = By.cssSelector("[data-qa=\"lufexuloga\"]");
+
+
+
+    // Page Actions
+    public String loggedInUserName() {
+        presenceOfElement(userNameOnDashboard);
+        return getElement(userNameOnDashboard).getText();
+    }
+
+}
+
---
+++ src/main/java/org/WebAutomation/utils/ExcelReader.java
@@ -0,0 +1,4 @@
+package org.WebAutomation.utils;
+
+public class ExcelReader {
+}
